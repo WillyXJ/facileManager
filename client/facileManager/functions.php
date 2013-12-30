d43ea4537ceb89c3437cea46d9bf2198e2ef481a
WillyXJ <jon@iMac.local>
2013-12-30 15:54:13 -0700

    Fixed an issue where client installation would fail when checking account details.

--- client/facileManager/functions.php
+++ client/facileManager/functions.php
@@ -339,10 +339,10 @@ function getPostData($url, $data) {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_POST, true);
-//	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	$result = curl_exec($ch);
 	curl_close($ch);
 	return $result;
--- server/fm-modules/facileManager/functions.php
+++ server/fm-modules/facileManager/functions.php
@@ -701,6 +701,7 @@ function getPostData($url, $data) {
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	$result = curl_exec($ch);
 	curl_close($ch);
 	return $result;
