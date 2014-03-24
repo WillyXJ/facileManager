7c3c6c7f01d044a98b694c92e6d9d9c9ee13c59f
WillyXJ <jon@iMac.local>
2014-03-23 21:32:53 -0600

    Version bump

--- server/fm-includes/version.php
+++ server/fm-includes/version.php
@@ -24,14 +24,14 @@
  *
  * @global string $fm_version
  */
-$fm_version = '1.1';
+$fm_version = '1.1.1';
 
 /**
  * Holds the facileManager DB revision, increments when changes are made to the facileManager DB schema.
  *
  * @global int $fm_db_version
  */
-$fm_db_version = 30;
+$fm_db_version = 31;
 
 /**
  * Holds the required PHP version
