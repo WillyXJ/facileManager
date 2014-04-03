3259fc6aa3bffe7de58767b876faa84f463fadc8
WillyXJ <jon@iMac.local>
2014-04-03 10:57:07 -0600

    Revert "Improved rewrite check"
    This reverts commit 430f5dfcb6f4d2bd212ed487c12346856da850c0.

--- server/fm-includes/init.php
+++ server/fm-includes/init.php
@@ -139,19 +139,17 @@ RewriteRule . index.php [L]
 	
 	/** Test rewrites */
 	if (!defined('INSTALL')) {
-		if (dns_get_record($_SERVER['SERVER_NAME'], DNS_A + DNS_AAAA)) {
-			$test_output = getPostData($GLOBALS['FM_URL'] . 'admin-accounts?verify', array('module_type' => 'CLIENT'));
-			$test_output = isSerialized($test_output) ? unserialize($test_output) : $test_output;
-			if (strpos($test_output, 'Account is not found.') === false) {
-				if ($single_check) {
-					bailOut(sprintf('<p style="text-align: center;">The required .htaccess file appears to not work with your Apache configuration which is required by %1s.</p>', $fm_name));
-				} else {
-					$requirement_check .= displayProgress('Test Rewrites', false, false);
-					$error = true;
-				}
+		$test_output = getPostData($GLOBALS['FM_URL'] . 'admin-accounts?verify', array('module_type' => 'CLIENT'));
+		$test_output = isSerialized($test_output) ? unserialize($test_output) : $test_output;
+		if (strpos($test_output, 'Account is not found.') === false) {
+			if ($single_check) {
+				bailOut(sprintf('<p style="text-align: center;">The required .htaccess file appears to not work with your Apache configuration which is required by %1s.</p>', $fm_name));
 			} else {
-				if (!$single_check) $requirement_check .= displayProgress('Test Rewrites', true, false);
+				$requirement_check .= displayProgress('Test Rewrites', false, false);
+				$error = true;
 			}
+		} else {
+			if (!$single_check) $requirement_check .= displayProgress('Test Rewrites', true, false);
 		}
 	}
 	
