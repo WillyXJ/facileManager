6f27b5c7c90f7c00ee0eb417501080df180254f5
WillyXJ <jon@iMac.local>
2014-04-25 17:35:33 -0600

    Minor tweaks

--- server/fm-modules/facileManager/pages/404.php
+++ server/fm-modules/facileManager/pages/404.php
@@ -30,13 +30,11 @@
 
 printHeader('File Not Found', 'install');
 
-$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $GLOBALS['RELPATH'];
-
 echo <<<HTML
 <div id="message"><p class="failed">file not found</p></div>
 <p>The file you tried ({$_SERVER['REQUEST_URI']}) is not found at this location.  The URL or link may be outdated or incorrect.</p>
 <p>If you typed the URL in the address bar, please make sure the spelling is correct.</p>
-<p id="forgotton_link"><a href="$referrer">&larr; Back</a></p>
+<p id="forgotton_link"><a href="javascript:history.back();">&larr; Back</a></p>
 
 HTML;
 
