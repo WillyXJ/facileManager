ae52a08a2614c78b65d861467028dccb94bf755a
WillyXJ <jon@iMac.local>
2015-03-11 22:30:54 -0600

    version bump

--- server/fm-includes/version.php
+++ server/fm-includes/version.php
@@ -24,14 +24,14 @@
  *
  * @global string $fm_version
  */
-$fm_version = '2.0-rc1';
+$fm_version = '2.0-rc2';
 
 /**
  * Holds the facileManager DB revision, increments when changes are made to the facileManager DB schema.
  *
  * @global int $fm_db_version
  */
-$fm_db_version = 40;
+$fm_db_version = 41;
 
 /**
  * Holds the required PHP version
--- server/fm-modules/fmDNS/variables.inc.php
+++ server/fm-modules/fmDNS/variables.inc.php
@@ -31,12 +31,12 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Information */
 $__FM_CONFIG['fmDNS'] = array(
-		'version'							=> '2.0-rc1',
-		'client_version'					=> '2.0-rc1',
+		'version'							=> '2.0-rc2',
+		'client_version'					=> '2.0-rc2',
 		'description'						=> _('Easily manage one or more ISC BIND servers through a web interface. No more editing configuration and zone files manually.'),
 		'prefix'							=> 'dns_',
 		'required_dns_version'				=> '9.3',
-		'required_fm_version'				=> '2.0-rc1',
+		'required_fm_version'				=> '2.0-rc2',
 		'min_client_auto_upgrade_version'	=> '1.2.5'
 	);
 
