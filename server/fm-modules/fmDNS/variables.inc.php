6b5893c14cf91aabfa1eaea58c2eb93890f6d6e8
WillyXJ <jon@iMac.local>
2015-05-03 15:17:36 -0600

    version bump

--- server/fm-modules/fmDNS/variables.inc.php
+++ server/fm-modules/fmDNS/variables.inc.php
@@ -31,12 +31,12 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Information */
 $__FM_CONFIG['fmDNS'] = array(
-		'version'							=> '2.0',
-		'client_version'					=> '2.0',
+		'version'							=> '2.0.1',
+		'client_version'					=> '2.0.1',
 		'description'						=> __('Easily manage one or more ISC BIND servers through a web interface. No more editing configuration and zone files manually.', 'fmDNS'),
 		'prefix'							=> 'dns_',
 		'required_dns_version'				=> '9.3',
-		'required_fm_version'				=> '2.0',
+		'required_fm_version'				=> '2.0.1',
 		'min_client_auto_upgrade_version'	=> '1.2.5'
 	);
 
