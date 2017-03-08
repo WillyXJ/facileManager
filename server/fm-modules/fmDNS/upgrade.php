3b7da60b9994638bfada153b083ff3e197df3f2c
WillyXJ <jon@iMac.local>
2017-03-08 12:15:48 -0700

    version bump

--- server/fm-modules/fmDNS/upgrade.php
+++ server/fm-modules/fmDNS/upgrade.php
@@ -30,7 +30,7 @@ function upgradefmDNSSchema($module_name) {
 	$running_version = getOption('version', 0, $module_name);
 	
 	/** Checks to support older versions (ie n-3 upgrade scenarios */
-	$success = version_compare($running_version, '3.0-alpha3', '<') ? upgradefmDNS_3003($__FM_CONFIG, $running_version) : true;
+	$success = version_compare($running_version, '3.0-beta1', '<') ? upgradefmDNS_3004($__FM_CONFIG, $running_version) : true;
 	if (!$success) return $fmdb->last_error;
 	
 	setOption('client_version', $__FM_CONFIG['fmDNS']['client_version'], 'auto', false, 0, 'fmDNS');
@@ -1918,7 +1918,7 @@ function upgradefmDNS_3003($__FM_CONFIG, $running_version) {
 	return true;
 }
 
-/** 3.0-alpha4 */
+/** 3.0-beta1 */
 function upgradefmDNS_3004($__FM_CONFIG, $running_version) {
 	global $fmdb, $module_name;
 	
