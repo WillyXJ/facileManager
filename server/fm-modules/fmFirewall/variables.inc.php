8d347402e4a848d624981b60d31189b915c81234
WillyXJ <jon@facilemanager.com>
2017-03-09 15:32:58 -0700

    fmFirewall - replaced policy icons

--- server/fm-modules/fmFirewall/variables.inc.php
+++ server/fm-modules/fmFirewall/variables.inc.php
@@ -41,8 +41,6 @@ $__FM_CONFIG['fmFirewall'] = array(
 
 /** Images */
 if (isset($__FM_CONFIG['module']['path'])) {
-	$__FM_CONFIG['module']['icons']['action']['active']		= '<img src="' . $__FM_CONFIG['module']['path']['images'] . '/__action__.png" border="0" alt="__Action__" title="__Action__" width="12" />';
-	$__FM_CONFIG['module']['icons']['action']['disabled']	= '<img src="' . $__FM_CONFIG['module']['path']['images'] . '/__action___d.png" border="0" alt="__Action__ (' . __('disabled') . ')" title="__Action__ (' . __('disabled') . ')" width="12" />';
 	$__FM_CONFIG['module']['icons']['action']['pass']		= '<i class="fa fa-arrow-up __action__" alt="__Action__" title="__Action__"></i>';
 	$__FM_CONFIG['module']['icons']['action']['block']		= '<i class="fa fa-times __action__" alt="__Action__" title="__Action__"></i>';
 	$__FM_CONFIG['module']['icons']['action']['reject']		= '<i class="fa fa-times __action__" alt="__Action__" title="__Action__"></i>';
