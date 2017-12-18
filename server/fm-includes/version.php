ebe881972ff22441bc97f1848f7b67ce266dfa3c
WillyXJ <support@facilemanager.com>
2017-12-18 09:05:08 -0700

    #405 - Fixed username display in logs

--- server/fm-includes/version.php
+++ server/fm-includes/version.php
@@ -24,14 +24,14 @@
  *
  * @global string $fm_version
  */
-$fm_version = '3.1';
+$fm_version = '3.1.1';
 
 /**
  * Holds the facileManager DB revision, increments when changes are made to the facileManager DB schema.
  *
  * @global int $fm_db_version
  */
-$fm_db_version = 46;
+$fm_db_version = 47;
 
 /**
  * Holds the required PHP version
