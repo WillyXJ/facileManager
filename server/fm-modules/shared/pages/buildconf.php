cd1064e7c5dc793430be4e72eeb09aa81f91809a
WillyXJ <support@facilemanager.com>
2018-02-27 13:50:53 -0700

    fmDNS - Fixed a fatal error during client interactions

--- server/fm-modules/shared/pages/buildconf.php
+++ server/fm-modules/shared/pages/buildconf.php
@@ -28,6 +28,7 @@ if (!defined('CLIENT')) define('CLIENT', true);
 require_once('fm-init.php');
 include(ABSPATH . 'fm-modules/facileManager/classes/class_accounts.php');
 include(ABSPATH . 'fm-modules/' . $_POST['module_name'] . '/classes/class_buildconf.php');
+include(ABSPATH . 'fm-modules/' . $_POST['module_name'] . '/classes/class_servers.php');
 
 /** Validate daemon version */
 if (array_key_exists('action', $_POST) && $_POST['action'] == 'version_check') {
