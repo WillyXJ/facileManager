bf4099491edf89bd4cf2aa1f089ba9886cdcbd9a
WillyXJ <support@facilemanager.com>
2018-07-26 16:18:18 -0600

    version bump

--- server/fm-modules/fmDNS/functions.php
+++ server/fm-modules/fmDNS/functions.php
@@ -1,7 +1,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2013-2018 The facileManager Team                               |
+ | Copyright (C) 2013-2018 The facileManager Team                          |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
