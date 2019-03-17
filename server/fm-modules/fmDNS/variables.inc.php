d36944c2542f3362c7a647cd56b41abfb14e73ea
WillyXJ <support@facilemanager.com>
2019-03-17 13:03:30 -0600

    fmDNS - version bump

--- server/fm-modules/fmDNS/variables.inc.php
+++ server/fm-modules/fmDNS/variables.inc.php
@@ -31,7 +31,7 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Information */
 $__FM_CONFIG['fmDNS'] = array(
-		'version'							=> '3.3',
+		'version'							=> '3.3.1',
 		'client_version'					=> '3.3',
 		'description'						=> __('Easily manage one or more ISC BIND servers through a web interface. No more editing configuration and zone files manually.', 'fmDNS'),
 		'prefix'							=> 'dns_',
