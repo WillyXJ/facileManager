e82d541a3919063e7aef6dc3e1932dcc91547b32
WillyXJ <support@facilemanager.com>
2019-03-20 19:31:31 -0600

    version bump

--- server/fm-modules/fmWifi/variables.inc.php
+++ server/fm-modules/fmWifi/variables.inc.php
@@ -31,7 +31,7 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Version */
 $__FM_CONFIG['fmWifi'] = array(
-		'version'							=> '0.2',
+		'version'							=> '0.2.1',
 		'client_version'					=> '0.2',
 		'description'						=> __('Manage wifi access points with hostapd.', 'fmWifi'),
 		'prefix'							=> 'wifi_',
