5340d326b45eb5001099e6a6550ae5f17f85a698
WillyXJ <support@facilemanager.com>
2020-11-11 09:08:44 -0700

    version bump

--- server/fm-modules/fmDNS/upgrade.php
+++ server/fm-modules/fmDNS/upgrade.php
@@ -32,7 +32,7 @@ function upgradefmDNSSchema($running_version) {
 	}
 	
 	/** Checks to support older versions (ie n-3 upgrade scenarios */
-	$success = version_compare($running_version, '4.0.0-beta2', '<') ? upgradefmDNS_4002($__FM_CONFIG, $running_version) : true;
+	$success = version_compare($running_version, '4.0.0', '<') ? upgradefmDNS_400($__FM_CONFIG, $running_version) : true;
 	if (!$success) return $fmdb->last_error;
 	
 	setOption('client_version', $__FM_CONFIG['fmDNS']['client_version'], 'auto', false, 0, 'fmDNS');
@@ -2335,8 +2335,8 @@ INSERTSQL;
 	return true;
 }
 
-/** 4.0.0-beta2 */
-function upgradefmDNS_4002($__FM_CONFIG, $running_version) {
+/** 4.0.0 */
+function upgradefmDNS_400($__FM_CONFIG, $running_version) {
 	global $fmdb;
 	
 	$success = version_compare($running_version, '4.0.0-beta1', '<') ? upgradefmDNS_4001($__FM_CONFIG, $running_version) : true;
@@ -2351,7 +2351,7 @@ function upgradefmDNS_4002($__FM_CONFIG, $running_version) {
 		}
 	}
 
-	setOption('version', '4.0.0-beta2', 'auto', false, 0, 'fmDNS');
+	setOption('version', '4.0.0', 'auto', false, 0, 'fmDNS');
 	
 	return true;
 }
