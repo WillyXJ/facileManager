9802d97d884d40e52d90a19f9bde0367b8e6ba3d
WillyXJ <support@facilemanager.com>
2020-12-02 09:38:59 -0700

    version bump

--- server/fm-includes/version.php
+++ server/fm-includes/version.php
@@ -24,14 +24,14 @@
  *
  * @global string $fm_version
  */
-$fm_version = '4.0.1';
+$fm_version = '4.0.2';
 
 /**
  * Holds the facileManager DB revision, increments when changes are made to the facileManager DB schema.
  *
  * @global int $fm_db_version
  */
-$fm_db_version = 48;
+$fm_db_version = 49;
 
 /**
  * Holds the required PHP version
