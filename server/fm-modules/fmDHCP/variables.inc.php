bae61836284dd33ea12a4163c6d209a5e1f81f55
WillyXJ <support@facilemanager.com>
2021-09-14 14:06:28 -0600

    version bump

--- server/fm-modules/fmDHCP/variables.inc.php
+++ server/fm-modules/fmDHCP/variables.inc.php
@@ -36,7 +36,7 @@ $__FM_CONFIG['fmDHCP'] = array(
 		'description'						=> __('Easily manage one or more ISC DHCP servers through a web interface. No longer edit configuration files manually.', 'fmDHCP'),
 		'prefix'							=> 'dhcp_',
 		'required_daemon_version'			=> '4.1',
-		'required_fm_version'				=> '4.0.1',
+		'required_fm_version'				=> '4.1.0',
 		'min_client_auto_upgrade_version'	=> '0.1'
 	);
 
