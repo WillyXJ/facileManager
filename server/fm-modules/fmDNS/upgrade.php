d0d9002247236659b25f1c312d3898ab33f795b8
WillyXJ <WillyXJ@users.noreply.github.com>
2021-09-29 08:27:37 -0600

    version bump

--- server/fm-modules/fmDNS/upgrade.php
+++ server/fm-modules/fmDNS/upgrade.php
@@ -32,7 +32,7 @@ function upgradefmDNSSchema($running_version) {
 	}
 	
 	/** Checks to support older versions (ie n-3 upgrade scenarios */
-	$success = version_compare($running_version, '5.0.0', '<') ? upgradefmDNS_500($__FM_CONFIG, $running_version) : true;
+	$success = version_compare($running_version, '5.1.0', '<') ? upgradefmDNS_510($__FM_CONFIG, $running_version) : true;
 	if (!$success) return $fmdb->last_error;
 	
 	setOption('client_version', $__FM_CONFIG['fmDNS']['client_version'], 'auto', false, 0, 'fmDNS');
