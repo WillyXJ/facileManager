8cec02ec74b5902eae575541b5ef79ce3978c290
WillyXJ <WillyXJ@users.noreply.github.com>
2023-03-09 12:07:26 -0700

    fmDNS - #572 - file not found recursion fix

--- server/fm-modules/fmDNS/classes/class_buildconf.php
+++ server/fm-modules/fmDNS/classes/class_buildconf.php
@@ -862,7 +862,7 @@ class fm_module_buildconf extends fm_shared_module_buildconf {
 			((`domain_name_servers`='0' OR `domain_name_servers`='s_{$server_id}' OR `domain_name_servers` LIKE 's_{$server_id};%' OR `domain_name_servers` LIKE '%;s_{$server_id};%' OR `domain_name_servers` LIKE '%;s_{$server_id}' $group_sql))
 			 $view_sql ORDER BY `domain_dnssec_parent_domain_id` DESC,`domain_clone_domain_id`,`domain_name` ASC";
 		$result = $fmdb->query($query);
-		if ($fmdb->num_rows) {
+		if ($fmdb->num_rows || $include_hint_zone) {
 			$count = $fmdb->num_rows;
 			$zone_result = $fmdb->last_result;
 
