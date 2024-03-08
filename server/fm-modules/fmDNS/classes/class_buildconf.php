49a5dc85c5a1b7028f3f26909463721d7ab18130
Gianluca Giacometti <52405+gianlucagiacometti@users.noreply.github.com>
2024-03-08 09:09:35 +0100

    Better fix of #617

--- server/fm-modules/fmDNS/classes/class_buildconf.php
+++ server/fm-modules/fmDNS/classes/class_buildconf.php
@@ -1339,9 +1339,9 @@ class fm_module_buildconf extends fm_shared_module_buildconf {
 				}
 				$record_start = str_pad($record_name, 25) . $separator . $record_ttl . $separator . $record_result[$i]->record_class . $separator . $record_result[$i]->record_type;
 
-				// Swap @ for domain_name in values except for TXT records
-				if ($record_result[$i]->record_type != 'TXT') {
-					$record_result[$i]->record_value = str_replace('@', $domain_name, $record_result[$i]->record_value);
+				// Swap @ for domain_name when the record value is '@'
+				if (trim($record_result[$i]->record_value) == '@') {
+					$record_result[$i]->record_value = $domain_name;
 				}
 				
 				switch($record_result[$i]->record_type) {
