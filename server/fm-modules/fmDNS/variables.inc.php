3d56f73c3edd4ed9a1d801a1c29b69f99c177581
WillyXJ <WillyXJ@users.noreply.github.com>
2024-10-07 12:43:40 -0600

    fmDNS - version bump

--- server/fm-modules/fmDNS/variables.inc.php
+++ server/fm-modules/fmDNS/variables.inc.php
@@ -31,7 +31,7 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Information */
 $__FM_CONFIG['fmDNS'] = array(
-		'version'							=> '6.1.0',
+		'version'							=> '6.1.1',
 		'client_version'					=> '6.1.0',
 		'description'						=> __('Easily manage one or more ISC BIND servers through a web interface. No more editing configuration and zone files manually.', 'fmDNS'),
 		'prefix'							=> 'dns_',
