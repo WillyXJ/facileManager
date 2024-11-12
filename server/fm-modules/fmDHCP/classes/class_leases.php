95442a0de8a64ec40caf8884df4b0398d6b4d11a
WillyXJ <WillyXJ@users.noreply.github.com>
2024-11-12 15:08:38 -0700

    fmDHCP - view lease dup reservation fix

--- server/fm-modules/fmDHCP/classes/class_leases.php
+++ server/fm-modules/fmDHCP/classes/class_leases.php
@@ -127,7 +127,14 @@ class fm_dhcp_leases {
 		extract($lease);
 		
 		if (currentUserCan('manage_leases', $_SESSION['module'])) {
-			$edit_status .= '<a href="#" class="edit_form_link" name="reserve_address" title="' . __('Tag as a fixed address (reservation)') . '"><i class="fa fa-tag" aria-hidden="true"></i></a> ';
+			/** Is this address already a defined host? */
+			if ($hardware) {
+				basicGet('fm_' . $__FM_CONFIG[$_SESSION['module']]['prefix'] . 'config', 'hardware', 'config_', 'config_name', "AND config_type='host' AND config_is_parent='no' AND config_data LIKE '%{$hardware}'");
+				if (!$fmdb->num_rows) {
+					$edit_status .= '<a href="#" class="edit_form_link" name="reserve_address" title="' . __('Tag as a fixed address (reservation)') . '"><i class="fa fa-tag" aria-hidden="true"></i></a> ';
+				}
+			}
+
 			$edit_status .= '<a href="#" class="delete">' . $__FM_CONFIG['icons']['delete'] . '</a>';
 			$edit_status = '<td id="row_actions">' . $edit_status . '</td>';
 			$checkbox = '<td><input type="checkbox" name="bulk_list[]" value="' . $ip .'" /></td>';
