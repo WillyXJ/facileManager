45778cc561bb8edf3bc64e2ab51333bb169620e2
WillyXJ <WillyXJ@users.noreply.github.com>
2025-02-01 21:31:13 -0700

    fmFirewall - CSS tweak

--- server/fm-modules/fmFirewall/variables.inc.php
+++ server/fm-modules/fmFirewall/variables.inc.php
@@ -44,10 +44,10 @@ $__FM_CONFIG['homepage'] = 'config-servers.php';
 
 /** Images */
 if (isset($__FM_CONFIG['module']['path'])) {
-	$__FM_CONFIG['module']['icons']['action']['pass']		= sprintf('<span class="tooltip-bottom" data-tooltip="%s"><i class="fa fa-arrow-up __action__" aria-hidden="true"></i></span>', __('Pass the packet'));
-	$__FM_CONFIG['module']['icons']['action']['block']		= sprintf('<span class="tooltip-bottom" data-tooltip="%s"><i class="fa fa-times __action__" aria-hidden="true"></i></span>', __('Block the packet'));
-	$__FM_CONFIG['module']['icons']['action']['reject']		= sprintf('<span class="tooltip-bottom" data-tooltip="%s"><i class="fa fa-times __action__" aria-hidden="true"></i></span>', __('Reject the packet'));
-	$__FM_CONFIG['module']['icons']['action']['log']		= sprintf('<span class="tooltip-bottom" data-tooltip="%s"><i class="fa fa-file-text-o __action__" aria-hidden="true"></i></span>', __('Log the packet'));
+	$__FM_CONFIG['module']['icons']['action']['pass']		= sprintf('<span class="tooltip-bottom mini-icon" data-tooltip="%s"><i class="fa fa-arrow-up __action__" aria-hidden="true"></i></span>', __('Pass the packet'));
+	$__FM_CONFIG['module']['icons']['action']['block']		= sprintf('<span class="tooltip-bottom mini-icon" data-tooltip="%s"><i class="fa fa-times __action__" aria-hidden="true"></i></span>', __('Block the packet'));
+	$__FM_CONFIG['module']['icons']['action']['reject']		= sprintf('<span class="tooltip-bottom mini-icon" data-tooltip="%s"><i class="fa fa-times __action__" aria-hidden="true"></i></span>', __('Reject the packet'));
+	$__FM_CONFIG['module']['icons']['action']['log']		= sprintf('<span class="tooltip-bottom mini-icon" data-tooltip="%s"><i class="fa fa-file-text-o __action__" aria-hidden="true"></i></span>', __('Log the packet'));
 	$__FM_CONFIG['module']['icons']['negated']				= sprintf('<span class="tooltip-bottom" data-tooltip="%s"><i class="fa fa-exclamation-circle block" aria-hidden="true"></i></span>', __('Negated'));
 	$__FM_CONFIG['module']['icons']['search']				= sprintf('<a href="#" class="global-search"><i class="fa fa-search preview" alt="%1$s" title="%1$s" aria-hidden="true"></i></a>', __('Global Search'));
 }
