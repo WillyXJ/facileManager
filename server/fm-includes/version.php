79e636f8e06d9ed9a68c321600af50175db56533
WillyXJ <WillyXJ@users.noreply.github.com>
2025-02-07 16:55:28 -0700

    version bump

--- server/fm-includes/version.php
+++ server/fm-includes/version.php
@@ -24,14 +24,14 @@
  *
  * @global string $fm_version
  */
-$fm_version = '5.1.1';
+$fm_version = '5.1.2';
 
 /**
  * Holds the facileManager DB revision, increments when changes are made to the facileManager DB schema.
  *
  * @global int $fm_db_version
  */
-$fm_db_version = 56;
+$fm_db_version = 57;
 
 /**
  * Holds the required PHP version
