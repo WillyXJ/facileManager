97a504204a200066690d8833f5bfd2aaa57ebdc6
WillyXJ <WillyXJ@users.noreply.github.com>
2025-02-21 07:54:50 -0700

    version bump

--- server/fm-modules/fmFirewall/variables.inc.php
+++ server/fm-modules/fmFirewall/variables.inc.php
@@ -31,7 +31,7 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Version */
 $__FM_CONFIG['fmFirewall'] = array(
-		'version'							=> '3.2.1',
+		'version'							=> '3.2.2',
 		'client_version'					=> '3.2.0',
 		'description'						=> __('Managing software firewalls should not be difficult. Manage one or more software firewall servers (iptables, ipfw, ipf, pf) through a web interface rather than configuration files individually.', 'fmFirewall'),
 		'prefix'							=> 'fw_',
