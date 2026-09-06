d43981235ef94c3b668badf8fadf3e28f1f08291
WillyXJ <WillyXJ@users.noreply.github.com>
2026-09-06 09:23:12 -0600

    version bump

--- server/fm-includes/version.php
+++ server/fm-includes/version.php
@@ -24,7 +24,7 @@
  *
  * @global string $fm_version
  */
-$fm_version = '6.0.7';
+$fm_version = '6.0.8';
 
 /**
  * Holds the facileManager DB revision, increments when changes are made to the facileManager DB schema.
--- server/fm-modules/fmWifi/variables.inc.php
+++ server/fm-modules/fmWifi/variables.inc.php
@@ -31,7 +31,7 @@ if (!@is_array($__FM_CONFIG)) $__FM_CONFIG = array();
 
 /** Module Version */
 $__FM_CONFIG['fmWifi'] = array(
-		'version'							=> '0.9.3',
+		'version'							=> '0.9.4',
 		'client_version'					=> '0.9.3',
 		'description'						=> __('Manage wifi access points with hostapd.', 'fmWifi'),
 		'prefix'							=> 'wifi_',
